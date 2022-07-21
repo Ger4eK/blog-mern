9148645b3f777d0513aa5dc568bf00ad3ec5d167
Ger4eK <ger4ek228@gmail.com>
2022-07-21 14:10:29 +0300

    added UserModel & Express Validator

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { registerValidation } from './validations/auth.js';
+import { validationResult } from 'express-validator';
 
 mongoose
   .connect(
@@ -12,21 +14,13 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.post('/auth/login', (req, res) => {
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: 'Дай боже!!!',
-    },
-    'secret123'
-  );
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
   res.json({
     success: true,
-    token,
   });
 });
 

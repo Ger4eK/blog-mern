af2eab37202249f619d12a3f2550eb7af5b92fd2
Ger4eK <ger4ek228@gmail.com>
2022-07-21 12:28:37 +0300

    added POST req & json format for the express app

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/auth/login', (req, res) => {
+  console.log(req.body);
+  res.json({
+    success: true,
+  });
 });
 
 app.listen(4444, (err) => {

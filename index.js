2c6024644476033f3224b01a7190e2e777b80979
Ger4eK <ger4ek228@gmail.com>
2022-07-21 13:20:36 +0300

    connecting MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:wwwwww@cluster0.axu866w.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
 
 const app = express();
 app.use(express.json());

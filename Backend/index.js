8ba0935311405cbda850fb16f92d7722e4638e18
Ger4eK <ger4ek228@gmail.com>
2022-07-22 12:42:03 +0300

    added trycatch for the error handling and JWT token

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ import UserModel from './models/User.js';
 
 mongoose
   .connect(
-    'mongodb+srv://admin:wwwwww@cluster0.axu866w.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://admin:wwwwww@cluster0.axu866w.mongodb.net/blog?retryWrites=true&w=majority'
   )
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
@@ -17,27 +17,48 @@ const app = express();
 app.use(express.json());
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    //! salt - це алгоритм завдяки якому буде зашифровуватись наш пароль
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    //! зберігаєм юзера в базі данних
+    const user = await doc.save();
+
+    //! після того як ми робимо реєстрацію ми будем повертати JWT
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    );
+
+    res.json({
+      ...user._doc,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Не вдалося зареєструватися',
+    });
   }
-
-  const password = req.body.password;
-  //! salt - це алгоритм завдяки якому буде зашифровуватись наш пароль
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  //! зберігаєм юзера в базі данних
-  const user = await doc.save();
-
-  res.json(user);
 });
 
 app.listen(4444, (err) => {

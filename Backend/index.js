e3b3ced09bc29f7b4e77f2ac2102d019b615ac27
Ger4eK <ger4ek228@gmail.com>
2022-07-22 12:14:35 +0300

    added User and BCrypt for password encryption

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import { registerValidation } from './validations/auth.js';
 import { validationResult } from 'express-validator';
+import UserModel from './models/User.js';
 
 mongoose
   .connect(
@@ -14,14 +16,28 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
-  res.json({
-    success: true,
+
+  const password = req.body.password;
+  //! salt - це алгоритм завдяки якому буде зашифровуватись наш пароль
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
   });
+
+  //! зберігаєм юзера в базі данних
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(4444, (err) => {

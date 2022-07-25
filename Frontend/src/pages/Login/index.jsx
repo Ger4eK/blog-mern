6afe32749ddb1e1c7c9919abefa961125758afc5
Ger4eK <ger4ek228@gmail.com>
2022-07-25 19:20:31 +0300

    Implementing auth with redux

--- Frontend/src/pages/Login/index.jsx
+++ Frontend/src/pages/Login/index.jsx
@@ -3,26 +3,63 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import { useForm } from 'react-hook-form';
 
 import styles from './Login.module.scss';
+import { useDispatch } from 'react-redux';
+import { fetchAuth } from '../../redux/slices/auth';
 
 export const Login = () => {
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: '228@gmail.com',
+      password: '12345',
+    },
+    mode: 'onChange',
+  });
+
+  const onSubmit = (values) => {
+    dispatch(fetchAuth(values));
+  };
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant='h5'>
-        Вхід в акаунт
+        Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label='E-Mail'
-        error
-        helperText='Неправильно вказана пошта'
-        fullWidth
-      />
-      <TextField className={styles.field} label='Пароль' fullWidth />
-      <Button size='large' variant='contained' fullWidth>
-        Увійти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label='E-Mail'
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          type='email'
+          {...register('email', { required: 'Укажите почту' })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label='Пароль'
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register('password', { required: 'Укажите пароль' })}
+          fullWidth
+        />
+        <Button
+          //disabled={!isValid}
+          type='submit'
+          size='large'
+          variant='contained'
+          fullWidth
+        >
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
